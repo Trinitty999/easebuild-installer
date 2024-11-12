9cc4119e5cec61d7edf2bf70312e223c7ec6df9d
tunafysh <hanansmani@gmail.com>
2024-11-12 14:35:35 +0100

    did some changes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,79 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::io::Write;
+use std::{env, fs};
+use std::process::Command;
+
 #[tauri::command]
-fn close() {
-    println!("Close");
+fn close(window: tauri::Window) {
+    window.close().unwrap();
+}
+
+#[tauri::command]
+fn minimize(window: tauri::Window) {
+    log::info!("minimized");
+    window.minimize().unwrap();
+}
+
+#[tauri::command]
+fn install() -> String {
+    log::info!("installed");
+    "installed".to_string()
+}
+
+#[tauri::command]
+fn repair() -> String {
+    log::info!("repaired");
+    "repaired".to_string()
+}
+
+
+#[tauri::command]
+fn init() {
+    let zipfile = include_bytes!("../../files.zip");
+    let tempdir = if env::consts::OS == "Windows" { "C:\\Windows\\Temp".to_string() } else { "/tmp".to_string() };
+    
+    let is_admin = if cfg!(target_os = "windows") {
+        let output = Command::new("net")
+        .args(&["session"])
+        .output()
+        .expect("failed to execute process");
+    output.status.success()
+    } else {
+        let output = Command::new("id")
+            .arg("-u")
+            .output()
+            .expect("failed to execute process");
+        let uid = String::from_utf8_lossy(&output.stdout).trim().to_string();
+        uid == "0"
+    };
+    
+    if is_admin {
+        fs::create_dir("installer_data").expect("Failed to initialize");
+        env::set_current_dir(format!("{}\\installer_data", &tempdir)).expect("Failed to change directory");
+        let mut files = fs::File::create("files.zip").expect("Failed to create file");
+        files.write_all(zipfile).expect("failed to extract installer files");
+    } else {
+        println!("Not running as administrator.");
+    }
+}
+
+#[tauri::command]
+fn uninstall() -> String {
+    log::info!("uninstalled");
+    "uninstalled".to_string()
+}
+
+#[tauri::command]
+fn toggle_devtools() {
+    log::info!("Can't do that :/");
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![close])
+        .invoke_handler(tauri::generate_handler![close, minimize, install, repair, uninstall, toggle_devtools, init])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 const { invoke } = window.__TAURI__.core;
+document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
 var cards = document.getElementsByClassName("cards")
 
 var mainmenu = document.getElementById("content")
@@ -33,77 +34,91 @@ document.addEventListener("keydown", (e) => {
     } 
     if (e.altKey == true && e.key == "i") 
     {
-        invoke("toggle-devtools")
+        invoke("toggle_devtools").then(() => {
+            console.log("Can't do that :/")
+        })
     } 
     if (e.ctrlKey == true && e.key == "i" || e.key == "I") 
     {
-        invoke("install")
-         window.ipcAPI.on("installed", (data) => {
-            cards[0].children[2].innerHTML = "Installed"
-            cards[2].children[2].innerHTML = "Uninstall"
-            setTimeout(() => {
-                cards[0].children[2].innerHTML = "Install"
-            }, 5000)
+        let res = invoke("install").then((text) => {
+
+            if (text == "installed") {
+                cards[0].children[2].innerHTML = "Installed"
+                cards[2].children[2].innerHTML = "Uninstall"
+                setTimeout(() => {
+                    cards[0].children[2].innerHTML = "Install"
+                }, 5000)
+            }
         })
     } 
     if (e.ctrlKey == true && e.key == "r" || e.key == "R") 
     {
-        window.ipcAPI.send("repair");
-        window.ipcAPI.on("repaired", (data) => {
-            cards[1].children[2].innerHTML = "Repaired"
-            setTimeout(() => {
-                cards[1].children[2].innerHTML = "Repair"
-            }, 5000)
-        })
-        window.ipcAPI.on("not-installed", (data) => {
-            alert("Not installed, please install first!")
+        let res = invoke("repair").then((text) => {
+
+            
+            if (text == "repaired"){
+                cards[1].children[2].innerHTML = "Repaired"
+                setTimeout(() => {
+                    cards[1].children[2].innerHTML = "Repair"
+                }, 5000)
+            }
+            else{
+                alert("Not installed, please install first!")
+            }
         })
     } 
     if (e.ctrlKey == true && e.key == "u" || e.key == "U") 
     {
-        window.ipcAPI.send("uninstall");
-        window.ipcAPI.on("uninstalled", (data) => {
-            cards[2].children[2].innerHTML = "Uninstalled"
-            cards[0].children[2].innerHTML = "Install"
-            setTimeout(() => {
-                cards[2].children[2].innerHTML = "Uninstall"
-            }, 5000)
+        let res = invoke("uninstall").then((text) => {
+
+            if (text == "uninstalled"){
+                cards[2].children[2].innerHTML = "Uninstalled"
+                cards[0].children[2].innerHTML = "Install"
+                setTimeout(() => {
+                    cards[2].children[2].innerHTML = "Uninstall"
+                }, 5000)
+            }
         })
     } 
 
 });
 
 cards[0].addEventListener('click', () => {
-    window.ipcAPI.send("install");
-    window.ipcAPI.on("installed", (data) => {
-        cards[0].children[2].innerHTML = "Installed"
-        cards[2].children[2].innerHTML = "Uninstall"
-        setTimeout(() => {
-            cards[0].children[2].innerHTML = "Install"
-        }, 5000)
+    let res = invoke("install").then((text) => {
+
+        if (text == "installed"){
+            cards[0].children[2].innerHTML = "Installed"
+            cards[2].children[2].innerHTML = "Uninstall"
+            setTimeout(() => {
+                cards[0].children[2].innerHTML = "Install"
+            }, 5000)
+        }
     })
 })
 
 cards[1].addEventListener('click', () => {
-    window.ipcAPI.send("repair");
-    window.ipcAPI.on("repaired", (data) => {
-        cards[1].children[2].innerHTML = "Repaired"
-        setTimeout(() => {
-            cards[1].children[2].innerHTML = "Repair"
-        }, 5000)
-    })
-    window.ipcAPI.on("not-installed", (data) => {
-        alert("Not installed, please install first!")
+    let res = invoke("repair").then((text) => {
+
+        if(text == "repaired"){
+            cards[1].children[2].innerHTML = "Repaired"
+            setTimeout(() => {
+                cards[1].children[2].innerHTML = "Repair"
+            }, 5000)
+        }
+        else{
+            alert("Not installed, please install first!")
+        }
     })
 })
 
 cards[2].addEventListener('click', () => {
-    window.ipcAPI.send("uninstall");
-    window.ipcAPI.on("uninstalled", (data) => {
-        cards[2].children[2].innerHTML = "Uninstalled"
-        cards[0].children[2].innerHTML = "Install"
-        setTimeout(() => {
-            cards[2].children[2].innerHTML = "Uninstall"
-        }, 5000)
+    let res = invoke("uninstall").then((text) => {
+        if(text == "uninstalled"){
+            cards[2].children[2].innerHTML = "Uninstalled"
+            cards[0].children[2].innerHTML = "Install"
+            setTimeout(() => {
+                cards[2].children[2].innerHTML = "Uninstall"
+            }, 5000)
+        }
     })
 })
